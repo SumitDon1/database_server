fc4e4c3cfe4d8fe2e95453ac576e3cdbcd72b8dc
MonkeyMan <110273989+SumitDon1@users.noreply.github.com>
2025-05-28 17:06:51 +0530

    Create cache22.h

---
+++ cache22/cache22.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
+void mainloop(void);
+int main(int ,char**)
