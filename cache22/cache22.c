1adb084856cb36c15e5ba035e385307439ef9e4b
MonkeyMan <110273989+SumitDon1@users.noreply.github.com>
2025-05-28 17:06:18 +0530

    Create cache22.c

---
+++ cache22/cache22.c
@@ -0,0 +1,20 @@
+#include "cache22.h"
+
+
+bool scontinuation;
+
+void mainloop(){
+    struct sockaddr_in sock;
+    int s;
+
+    
+}
+
+int main(int argc , char *argv[]){
+    scontinuation = true;
+    while(scontinuation){
+        mainloop();
+    }
+
+    return 0;
+}
